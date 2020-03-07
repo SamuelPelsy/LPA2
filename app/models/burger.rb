06a4516b0c6a4df20ea7a685fa9e62f57c5eca6c
sam <samuel.pelsy@outlook.fr>
2020-03-07 13:35:05 +0100

    je commence a comprendre pourquoi c'est trop bien les tests

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -2,12 +2,14 @@ class Burger < ApplicationRecord
     def get_nutriments
         res = Array.new
         res.push("Aucun nutriment")
-        product = Openfoodfacts::Product.get(self.code, locale: 'fr')
-        if(!product.nil.present?)
-            nutri = product.nutriments.to_hash
-            if nutri.count != 0
-                for ele in nutri
-                    res.push(ele[0].to_s + " : " + ele[1].to_s)
+        if(!self.code.nil?)
+            product = Openfoodfacts::Product.get(self.code, locale: 'fr') 
+            if(!product.nil.present?)
+                nutri = product.nutriments.to_hash
+                if nutri.count != 0
+                    for ele in nutri
+                        res.push(ele[0].to_s + " : " + ele[1].to_s)
+                    end
                 end
             end
         end
