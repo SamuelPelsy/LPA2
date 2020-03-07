e680f56cc8171e9568ff543a5e3b9d7ce5a97248
sam <samuel.pelsy@outlook.fr>
2020-03-07 13:27:41 +0100

    resolve view superfooderrorofthedeath

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,14 +1,14 @@
 class Burger < ApplicationRecord
     def get_nutriments
         res = Array.new
+        res.push("Aucun nutriment")
         product = Openfoodfacts::Product.get(self.code, locale: 'fr')
-        nutri = product.nutriments.to_hash
-
-        if nutri.count == 0
-            res.push("Aucun nutriment")
-        else
-            for ele in nutri
-                res.push(ele[0].to_s + " : " + ele[1].to_s)
+        if(!product.nil.present?)
+            nutri = product.nutriments.to_hash
+            if nutri.count != 0
+                for ele in nutri
+                    res.push(ele[0].to_s + " : " + ele[1].to_s)
+                end
             end
         end
         return res
