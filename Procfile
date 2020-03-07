233039cae00258fb311b8ba15945cc87f41f15ee
sam <samuel.pelsy@outlook.fr>
2020-03-07 12:09:50 +0100

    ajout du wihtout production

--- Procfile
+++ Procfile
@@ -1,2 +1,5 @@
-web: bundle exec puma -C config/puma.rb
-release: rake db:migrate
+# create db
+web: bin/rails db:migrate
+
+# launch server
+web: bin/rails server -p $PORT -e $RAILS_ENV
--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,2 +1,6 @@
 class Burger < ApplicationRecord
+    def nutriments
+        product = Openfoodfacts::Product.get(:code, locale: 'fr')
+        return product.nutriments.to_hash.count
+    end
 end
