f44c43ecbc9112e80c7ca93df81519f641a6c094
sam <samuel.pelsy@outlook.fr>
2020-03-07 11:48:29 +0100

    add code to burgers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_103512) do
+ActiveRecord::Schema.define(version: 2020_03_07_104410) do
 
   create_table "burgers", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_103512) do
     t.string "nutriscore"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "code"
   end
 
 end
